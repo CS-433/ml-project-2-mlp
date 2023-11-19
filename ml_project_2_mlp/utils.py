673fb701afbe1e3c5869f9ef9b71206dd81e6d45
Mika Senghaas <mail@mikasenghaas.de>
2023-11-19 18:26:57 +0100

    feat(*): added skeleton for module

---
+++ ml_project_2_mlp/utils.py
@@ -0,0 +1,11 @@
+"""
+Module containing utility functions.
+
+Functions:
+    check_import: Checks if the module can be imported.
+"""
+
+
+def check_import():
+    """Checks if the module can be imported."""
+    return True
