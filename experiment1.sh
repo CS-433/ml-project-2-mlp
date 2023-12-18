21e96aa501e2ebbeb5d50b226c131816f8731a3c
Mika Senghaas <mail@mikasenghaas.de>
2023-12-18 21:11:36 +0100

    feat(sh) updated experiment1 script

--- experiment1.sh
+++ experiment1.sh
@@ -5,45 +5,21 @@ with annotations from all GPT labelers and the crowdsourced human
 labels.
 '
 
-tuned=false
-group="exp1"
-
-# Pre-trained Homepage2Vec model
-poetry run train \
-    data=original \
-    labeler=human \
-    logger=wandb \
-    logger.wandb.name=$group-pretrained \
-    finetune=false \
-    eval=true \
-    group=$group \
-
-# Fine-tune on human labels
-poetry run train \
-    data=original \
-    labeler=human \
-    logger=wandb \
-    logger.wandb.name=$group-human \
-    eval=true \
-    group=$group
-
-# Fine-tune on GPT labels
-# labelers=$(ls data/labels)
 labelers=( 
+    "human"
     "gpt3.5-zeroshot-context1" 
     "gpt3.5-oneshot-context1" 
     "gpt3.5-zeroshot-context2" 
     "gpt3.5-oneshot-context2" 
     "gpt3.5-zeroshot-context3" 
     "gpt3.5-oneshot-context3" 
+    "gpt4-zeroshot-context2" 
+    "gpt4-oneshot-context2"
     )
+
+poetry run train experiment=exp1-pretrained
 
 for labeler in "${labelers[@]}"
 do
-    poetry run train \
-        data=original \
-        labeler=$labeler \
-        logger=wandb \
-        logger.wandb.name=$group-$labeler \
-        group=$group
+    poetry run train experiment=exp1-finetuned train_labeler=$labeler
 done
