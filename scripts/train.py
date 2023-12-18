17764eb60be0d5472a311cb0059ef6ca1957f4fe
Mika Senghaas <mail@mikasenghaas.de>
2023-12-18 21:12:07 +0100

    feat(train) train script uses different data/ labeler for train and eval

--- scripts/train.py
+++ scripts/train.py
@@ -7,12 +7,13 @@ from typing import List
 import hydra
 import lightning as L
 import rootutils
+import torch
+import wandb
 from lightning import Callback, LightningDataModule, LightningModule, Trainer
 from lightning.pytorch.loggers import Logger
 from omegaconf import DictConfig
 
 import ml_project_2_mlp.utils as utils
-import wandb
 from ml_project_2_mlp.data import WebsiteData
 from ml_project_2_mlp.labeler import WebsiteLabeler
 from ml_project_2_mlp.logger import RankedLogger
@@ -38,23 +39,67 @@ def main(cfg: DictConfig):
     if cfg.get("seed"):
         L.seed_everything(cfg.seed)
 
-    # Instantiate data
-    log.info(f"Instantiating data <{cfg.data._target_}>")
-    data: WebsiteData = hydra.utils.instantiate(cfg.data)
+    # Instantiate train data
+    log.info(f"Instantiating train data <{cfg.train_data._target_}>")
+    train_data: WebsiteData = hydra.utils.instantiate(cfg.train_data)
 
     # Instantiate labeler
-    log.info(f"Instantiating labeler <{cfg.labeler._target_}>")
-    labeler: WebsiteLabeler = hydra.utils.instantiate(cfg.labeler, data=data)
-
-    # Instantiate data module
-    log.info(f"Instantiating datamodule <{cfg.datamodule._target_}>")
-    datamodule: LightningDataModule = hydra.utils.instantiate(
-        cfg.datamodule, data=data, labeler=labeler
+    log.info(f"Instantiating train labeler <{cfg.train_labeler._target_}>")
+    train_labeler: WebsiteLabeler = hydra.utils.instantiate(
+        cfg.train_labeler, data=train_data
     )
+
+    # Instantiate train data module
+    log.info(f"Instantiating training datamodule <{cfg.train_datamodule._target_}>")
+    train_datamodule: LightningDataModule = hydra.utils.instantiate(
+        cfg.train_datamodule, data=train_data, labeler=train_labeler
+    )
+    train_datamodule.setup()
+
+    log.info(f"Instantiating test data <{cfg.test_data._target_}>")
+    test_data: WebsiteData = hydra.utils.instantiate(cfg.test_data)
+
+    # Instantiate labeler
+    log.info(f"Instantiating test labeler <{cfg.test_labeler._target_}>")
+    test_labeler: WebsiteLabeler = hydra.utils.instantiate(
+        cfg.test_labeler, data=test_data
+    )
+
+    log.info(f"Instantiating testing datamodule <{cfg.test_datamodule._target_}>")
+    test_datamodule: LightningDataModule = hydra.utils.instantiate(
+        cfg.test_datamodule, data=test_data, labeler=test_labeler
+    )
+    test_datamodule.setup()
 
     # Instantiate model module
     log.info(f"Instantiating model <{cfg.model._target_}>")
-    model: LightningModule = hydra.utils.instantiate(cfg.model)
+    train_data = train_datamodule.train_dataset.dataset
+    train_labels = train_data.labels
+    num_samples = len(train_labels)
+    class_counts = train_labels.sum(axis=0)
+    # num_samples = 886000
+    # train_ratio = torch.Tensor(
+    #     [
+    #         0.093,
+    #         0.276,
+    #         0.062,
+    #         0.017,
+    #         0.059,
+    #         0.015,
+    #         0.011,
+    #         0.011,
+    #         0.084,
+    #         0.043,
+    #         0.048,
+    #         0.074,
+    #         0.139,
+    #         0.068,
+    #     ]
+    # )
+    # class_counts = train_ratio * num_samples
+    pos_ratio = (num_samples - class_counts) / class_counts
+
+    model: LightningModule = hydra.utils.instantiate(cfg.model, pos_ratio=pos_ratio)
 
     # Instantiating callbacks for Lightning Trainer
     log.info("Instantiating callbacks...")
@@ -84,7 +129,11 @@ def main(cfg: DictConfig):
     # Train model
     if cfg.get("finetune"):
         log.info("Starting training!")
-        trainer.fit(model=model, datamodule=datamodule)
+        train_loader = train_datamodule.train_dataloader()
+        val_loader = test_datamodule.val_dataloader()
+        trainer.fit(
+            model=model, train_dataloaders=train_loader, val_dataloaders=val_loader
+        )
 
         train_metrics = trainer.callback_metrics
     else:
@@ -103,7 +152,12 @@ def main(cfg: DictConfig):
         if ckpt_path == "":
             log.warning("Best ckpt not found! Using current weights for testing...")
             ckpt_path = None
-        trainer.test(model=model, datamodule=datamodule, ckpt_path=ckpt_path)
+        test_loader = test_datamodule.test_dataloader()
+        trainer.test(
+            model=model,
+            dataloaders=test_loader,
+            ckpt_path=ckpt_path,
+        )
         test_metrics = trainer.callback_metrics
         log.info(f"Best ckpt path: {ckpt_path}")
     else:
